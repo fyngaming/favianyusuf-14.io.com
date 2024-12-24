9726970656cd65cca58fec5820672f3aa206608d
fyngaming <favianyusuf9@gmail.com>
2024-12-25 06:55:46 +0700

    Add files via upload

---
+++ coding/data.js
@@ -0,0 +1,10 @@
+const nama = "Favian yusuf";
+let usia = 30;
+
+function generateBiodata() {
+console.log('nama saya adalah ${nama} dan usia saya ${usia} tahun');
+}
+console.log(nama);
+console.log(usia);
+
+generateBiodata();
